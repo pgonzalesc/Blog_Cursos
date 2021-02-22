6fbbb2aa07a44f4447fd31b2b43a897efdee60b8
pgonzalesc <pedro281195@gmail.com>
2021-02-21 19:42:20 -0500

    Refactor: Validaciones con Form Request

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Curso;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreCurso;
 
 class CursoController extends Controller
 {
@@ -17,14 +18,7 @@ class CursoController extends Controller
         return view("cursos.create");
     }
 
-    public function store(Request $request) {
-
-        $request->validate([
-            'name' => 'required|max:10',
-            'description' => 'required|min:10',
-            'category' => 'required'
-        ]);
-
+    public function store(StoreCurso $request) {
         $curso = new Curso();
         $curso->name = $request->name;
         $curso->description = $request->description;
@@ -42,14 +36,7 @@ class CursoController extends Controller
         return view("cursos.edit", compact('curso'));
     }
 
-    public function update(Request $request, Curso $curso) {
-
-        $request->validate([
-            'name' => 'required|max:10',
-            'description' => 'required|min:10',
-            'category' => 'required'
-        ]);
-
+    public function update(StoreCurso $request, Curso $curso) {
         $curso->name = $request->name;
         $curso->description = $request->description;
         $curso->category = $request->category;
