42b842279a5efb4dac7d73d22bdeeb5d3da37d19
pgonzalesc <pedro281195@gmail.com>
2021-02-21 22:47:23 -0500

    Add: Se agregó el delete para el formulario

--- resources/views/cursos/show.blade.php
+++ resources/views/cursos/show.blade.php
@@ -10,4 +10,10 @@
     <a href="{{route('cursos.edit', $curso)}}">Editar Curso</a>
     <p><strong>Categoría: </strong>{{$curso->category}}</p>
     <p>{{$curso->description}}</p>
+
+    <form action="{{route('cursos.destroy', $curso)}}" method="POST">
+        @csrf
+        @method('delete')
+        <button type="submit">Eliminar</button>
+    </form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,5 @@ Route::get('cursos/{curso?}', [CursoController::class, 'show'])->name('cursos.sh
 Route::get('cursos/{curso}/edit', [CursoController::class, 'edit'])->name('cursos.edit');
 
 Route::put('cursos/{curso}', [CursoController::class, 'update'])->name('cursos.update');
+
+Route::delete('cursos/{curso}', [CursoController::class, 'destroy'])->name('cursos.destroy');
